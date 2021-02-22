de5fe14ebffff10aa2c48641493ded991e9d2bbf
maclan <maclanrowley42@gmail.com>
2021-02-22 10:13:21 +0000

    added map data with dummy data

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var port = 3000;
+var port = 80;
 var indexRouter = require('./routes/web');
 var usersRouter = require('./routes/users');
 
